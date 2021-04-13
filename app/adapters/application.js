26d11cbda3f90c10d3d790c7c64481ae4539053c
cegprakash <cegprakash@gmail.com>
2021-04-14 02:35:03 +0530

    added ember active model adapter

---
+++ app/adapters/application.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+import ActiveModelAdapter from 'active-model-adapter';
+
+
+export default ActiveModelAdapter.extend({
+    host: 'http://localhost:3000',
+    namespace: 'api/v1',
+});
--- app/routes/user/index.js
+++ app/routes/user/index.js
@@ -1,4 +1,9 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    model() {
+        let users = this.get('store').findAll('user');
+        console.log(users);
+        return users;
+    }
 });
