84e13c6b361ab471ff69ef59b47aa292b466575f
cegprakash <cegprakash@gmail.com>
2021-04-20 22:29:17 +0530

    added inverse releation

--- app/models/comment.js
+++ app/models/comment.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-    from_user_id: DS.belongsTo('user'),
-    to_user_id: DS.attr(),
+    from_user_id: DS.belongsTo('user', {'inverse': null}),
+    to_user_id: DS.belongsTo('user', {'inverse': null}),
     comment: DS.attr('string'),
     created_at: DS.attr('date'),
 });
